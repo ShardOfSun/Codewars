0dcd078bf65db6120c5c8789d461bca32b3e34af
Ternovskaya Elisaveta <ternovskayaliza2004@gmail.com>
2023-09-22 15:13:39 +0300

    Creating

--- src/Fighter.java
+++ src/Fighter.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Fighter {
+public class Fighter {
+    public String name;
+    public int health, damagePerAttack;
+    public Fighter(String name, int health, int damagePerAttack) {
+        this.name = name;
+        this.health = health;
+        this.damagePerAttack = damagePerAttack;
+    }
 }
